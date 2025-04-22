1a984540acf0a31ee5af00e84fbbeda1f5d854d6
Sergii <sergii@Julias-MacBook-Pro.local>
2025-04-22 20:06:09 +0300

    task solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const populationElements = document.body.querySelectorAll('span.population');
+
+const populationNumbers = [...populationElements]
+  .map((el) => +el.textContent.replaceAll(',', ''))
+  .filter((el) => !isNaN(el));
+
+const total = populationNumbers.reduce((sum, el) => sum + el, 0);
+const average = Math.round(total / populationNumbers.length);
+
+const formatter = new Intl.NumberFormat();
+
+const totalFormatted = formatter.format(total);
+const averageFormatted = formatter.format(average);
+
+document.querySelector('span.total-population').textContent = totalFormatted;
+
+document.querySelector('span.average-population').textContent =
+  averageFormatted;
